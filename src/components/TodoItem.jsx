19af8e6d2e8f65ea563c2e1808452f98a26d3a3d
t-bello7 <bello4aus@gmail.com>
2022-07-12 11:46:34 +0100

    add tenary operator to completed title

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import styles from "./TodoItem.module.css";
 
 class TodoItem extends React.Component {
     render(){
-        return <li> <input type="checkbox" checked={this.props.todo.completed} onChange={()=>this.props.handleChangeProps(this.props.todo.id)}/> {this.props.todo.title} <button onClick={()=> this.props.handleDeleteTodo(this.props.todo.id)}>Delete</button> </li>
+        const {completed, id, title} = this.props.todo;
+        const completedStyle = {
+            fontStyle: "italic",
+            color: "#595959",
+            opacity: 0.4,
+            textDecoration: "line-through",
+        }
+        return (<li className={styles.item}> 
+                <input type="checkbox" checked={completed} onChange={()=>this.props.handleChangeProps(id)}/> 
+                   <span style={completed ? completedStyle : null}>
+                        {title} 
+                    </span>
+                <button onClick={()=> this.props.handleDeleteTodo(id)}>Delete</button> 
+                </li>)
     }
 }
 
